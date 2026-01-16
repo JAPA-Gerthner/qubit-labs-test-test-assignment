c04624dcf85bfee687902cf5b396201cc9318614
Gerthner <chkalov.valerii.91@gmail.com>
2026-01-17 01:00:53 +0200

    Mobile responsiveness because fuck it why not at this point?

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,19 @@ import ResultsPanel from './components/ResultsPanel.vue'
   <div class="min-h-screen flex flex-col">
     <HeaderBar />
 
-    <main class="flex-1 flex gap-4 p-4">
+    <main class="flex-1 flex flex-col lg:flex-row gap-4 p-4">
       <!-- Left sidebar - Horse List -->
-      <div class="w-64 shrink-0">
+      <div class="w-full lg:w-64 shrink-0 order-2 lg:order-1">
         <HorseList />
       </div>
 
       <!-- Center - Race Track -->
-      <div class="flex-1">
+      <div class="flex-1 order-1 lg:order-2 min-h-[500px]">
         <RaceTrack />
       </div>
 
       <!-- Right sidebar - Results -->
-      <div class="w-72 shrink-0">
+      <div class="w-full lg:w-72 shrink-0 order-3">
         <ResultsPanel />
       </div>
     </main>
